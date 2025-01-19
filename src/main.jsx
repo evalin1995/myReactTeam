469b5b09657544547e3c84a55260c2039da55348
evalin1995 <nhglje250@gmail.com>
2025-01-19 22:16:31 +0800

    0119

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter basename={process.env.NODE_ENV === 'production' ? '/LIN16/' : '/'}>
+    {/* <HashRouter basename={process.env.NODE_ENV === 'production' ? '/LIN16/' : '/'}> */}
+   <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
